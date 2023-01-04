c117e3182dd89de88c087b9baa8d7766baa54da5
Felipe <cataofelipe@gmail.com>
2023-01-04 00:48:45 -0300

    Update

---
+++ Configurates.py
@@ -0,0 +1,39 @@
+from xml.dom import minidom
+import xml.etree.ElementTree as ET
+
+class Config:
+    def __init__(self) -> None:
+        ColorClass = []
+        pass
+    ColorClass = []
+    ThemeClass = []
+    def Readtheme(self):
+        tree = ET.parse('./MyBox/Theme.xml')
+        root = tree.getroot()
+        ThemeDefault = root.attrib
+        for child in root.findall("./"+ThemeDefault["name"]+"/ThemeConfig"):
+            self.ColorClass.append(child.attrib)
+        return self.ColorClass
+
+    def GetCurrentTheme(self):
+        tree = ET.parse('./MyBox/Theme.xml')
+        root = tree.getroot()
+        ThemeDefault = root.attrib
+        return ThemeDefault["name"]
+     
+    def ReadThemesInSystem(self):
+        tree = ET.parse('./MyBox/Theme.xml')
+        root = tree.getroot()
+        for child in root:
+            self.ThemeClass.append(child.tag)
+        pass
+
+    def ModifyTheme(self,Theme):
+        tree = ET.parse('./MyBox/Theme.xml')
+        root = tree.getroot()
+        ThemeDefault = root.attrib
+        print(ThemeDefault)
+        ThemeDefault["name"] = Theme
+        tree.write('./MyBox/Theme.xml')
+        pass
+
